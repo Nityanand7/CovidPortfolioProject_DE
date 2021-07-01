3f1cbe5e0c9c96316f042f98fb2c610205f9839b
Nityanand7 <39838213+Nityanand7@users.noreply.github.com>
2021-07-01 23:45:56 +0530

    Update CovidPortfolioProject_TableauVisualization.sql

--- CovidPortfolioProject_TableauVisualization.sql
+++ CovidPortfolioProject_TableauVisualization.sql
@@ -1,8 +1,8 @@
 /*
-Queries used for Tableau Project
+Queries used for Data Visualization
 */
 
---1
+--1. Global Numbers
 
 Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
 From PortfolioProj..CovidDeaths
@@ -12,7 +12,7 @@ where continent is not null
 order by 1,2
 
 
---2
+--2. Deaths Per Continent
 
 Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
 From PortfolioProj..CovidDeaths
@@ -23,7 +23,7 @@ Group by location
 order by TotalDeathCount desc
 
 
---3
+--3. Population Infected Per Country
 
 Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProj..CovidDeaths
@@ -32,7 +32,7 @@ Group by Location, Population
 order by PercentPopulationInfected desc
 
 
---4
+--4. Percent Population Infected
 
 Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
 From PortfolioProj..CovidDeaths
